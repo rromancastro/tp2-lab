6515ccba23147a7600bfc208a8ddca62d7244ca0
lucianoborsotto-dev <luciano.borsotto@gmail.com>
2026-08-27 19:20:33 -0300

    Create Bateria.java

---
+++ src/tp2/Bateria.java
@@ -0,0 +1,25 @@
+
+package tp2;
+
+/**
+ *
+ * @author lbors
+ */
+public class Bateria {
+    
+    
+     private double carga;
+    
+    public Bateria () {
+        this.carga = 2000;
+    }
+
+    public double getCarga() {
+        return carga;
+    }
+
+    public void setCarga(double carga) {
+        this.carga = carga;
+    }
+    
+}
