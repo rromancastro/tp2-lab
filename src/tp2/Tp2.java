0a3463755e1efa70a4cca4313eff11ae55c6cadd
Roman Castro <xxromanxxtm@gmail.com>
2026-08-27 19:22:45 -0300

    borro la calse y agrego sout

--- src/tp2/Tp2.java
+++ src/tp2/Tp2.java
@@ -1,15 +1,7 @@
 package tp2;
 public class Tp2 {
     public static void main(String[] args) {
-        Robot robot = new Robot();
-
-        Hombre hombre = new Hombre();
-
-        hombre.jugarConRobot(robot);  
-        
-        Hombre hombre2 = new Hombre();
-        
-        hombre2.jugarConRobot(robot);
+        System.out.println("holaaaaaaa luchiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiio");
     }
     
 }
