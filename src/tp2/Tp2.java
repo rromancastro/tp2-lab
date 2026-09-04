3d0a56fd1bf6106470d7df74dac11dfa13fee03e
Roman Castro <xxromanxxtm@gmail.com>
2026-09-03 21:00:50 -0300

    cambios finales

--- src/tp2/Tp2.java
+++ src/tp2/Tp2.java
@@ -1,4 +1,5 @@
 package tp2;
+
 public class Tp2 {
     public static void main(String[] args) {
         System.out.println("holaaaaaaa luchiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiio");
